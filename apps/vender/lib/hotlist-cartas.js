cb9d2bc41ccbbe92d0f1eec9d37a2fab8c866bfa
Luke Perez <lukegothic@gmail.com>
2026-09-14 09:29:34 +0200

    chore: refrescar los precios de referencia de la hotlist
    prices.eur venia del 08/09 y solo sirve de referencia al recotizar, asi que
    se baja de nuevo de Scryfall para las 22 entradas. No se toca ningun
    pagamos: la oferta la decide el negocio, no esta cifra, y las tres cartas
    sin precio siguen sin publicarse.
    
    Dos bajan lo bastante como para mirarlas: Lion's Eye Diamond un 6,9 % y
    Tundra un 6,1 %, que dejan su pagamos en el 64 % del mercado en vez del 60 %
    al que estan las demas.
    
    ACTUALIZADA se queda en 08/09/2026 porque ninguna oferta publicada ha
    cambiado. La fecha dice de cuando es lo que se paga, y prices.eur no llega
    nunca a la pagina.

--- apps/vender/lib/hotlist-cartas.js
+++ apps/vender/lib/hotlist-cartas.js
@@ -38,91 +38,91 @@ const CARTAS = [
     name: "Gaea's Cradle",
     set_name: "Urza's Saga",
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/2/5/25b0b816-0583-44aa-9dc5-f3ff48993a51.jpg' },
-    prices: { eur: '1160.65' },
+    prices: { eur: '1191.59' },
     pagamos: 695
   },
   {
     name: 'Mox Diamond',
     set_name: 'Stronghold',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/2/8/28028830-83ed-45e2-b495-3b9ad9d3e988.jpg' },
-    prices: { eur: '835.47' },
+    prices: { eur: '816.60' },
     pagamos: 500
   },
   {
     name: 'Underground Sea',
     set_name: 'Revised Edition',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/1/f/1f35877c-e66c-4ef0-842a-f68cd233ae4b.jpg' },
-    prices: { eur: '715.19' },
+    prices: { eur: '720.39' },
     pagamos: 430
   },
   {
     name: 'Volcanic Island',
     set_name: 'Revised Edition',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/b/1/b12e5430-0e80-47dd-80ac-85728b656a24.jpg' },
-    prices: { eur: '573.38' },
+    prices: { eur: '584.37' },
     pagamos: 345
   },
   {
     name: "Lion's Eye Diamond",
     set_name: 'Mirage',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/6/3/63bacc32-d6ba-420c-9b49-299c08e5fb39.jpg' },
-    prices: { eur: '535.71' },
+    prices: { eur: '498.50' },
     pagamos: 320
   },
   {
     name: 'Imperial Seal',
     set_name: 'Portal Three Kingdoms',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/8/2/822e30db-40c5-4099-868b-185ad9b7c7dc.jpg' },
-    prices: { eur: '395.86' },
+    prices: { eur: '385.11' },
     pagamos: 240
   },
   {
     name: 'Tundra',
     set_name: 'Revised Edition',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/9/c/9c9d5f72-e199-4d5b-ae7e-cc5b9bdfae99.jpg' },
-    prices: { eur: '391.71' },
+    prices: { eur: '367.77' },
     pagamos: 235
   },
   {
     name: 'Bayou',
     set_name: 'Revised Edition',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/5/6/56355ff3-2232-4a11-b868-aec9a50b9ee5.jpg' },
-    prices: { eur: '306.28' },
+    prices: { eur: '313.86' },
     pagamos: 185
   },
   {
     name: 'Grim Monolith',
     set_name: "Urza's Legacy",
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/9/d/9ddc9fe1-17c8-4e1d-aeb8-c4214e881280.jpg' },
-    prices: { eur: '309.74' },
+    prices: { eur: '308.62' },
     pagamos: 185
   },
   {
     name: 'The One Ring',
     set_name: 'The Lord of the Rings: Tales of Middle-earth',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/d/5/d5806e68-1054-458e-866d-1f2470f682b2.jpg' },
-    prices: { eur: '89.28' },
+    prices: { eur: '91.71' },
     pagamos: 53
   },
   {
     name: 'Sheoldred, the Apocalypse',
     set_name: 'Dominaria United',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/d/6/d67be074-cdd4-41d9-ac89-0a0456c4e4b2.jpg' },
-    prices: { eur: '75.78' },
+    prices: { eur: '77.06' },
     pagamos: 45
   },
   {
     name: 'Force of Will',
     set_name: 'Alliances',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/9/a/9a879b60-4381-447d-8a5a-8e0b6a1d49ca.jpg' },
-    prices: { eur: '59.62' },
+    prices: { eur: '60.62' },
     pagamos: 36
   },
   {
     name: 'Wasteland',
     set_name: 'Tempest',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/9/9/99ff731b-8399-40c8-b539-ba6ba5783771.jpg' },
-    prices: { eur: '46.68' },
+    prices: { eur: '44.59' },
     pagamos: 28
   },
   {
@@ -136,35 +136,35 @@ const CARTAS = [
     name: 'Jeweled Lotus',
     set_name: 'Commander Legends',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/3/c/3c7de64b-3dc8-47dd-8999-4353b5a3a06f.jpg' },
-    prices: { eur: '37.98' },
+    prices: { eur: '39.13' },
     pagamos: 23
   },
   {
     name: 'Cavern of Souls',
     set_name: 'Avacyn Restored',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/1/3/1381c8f1-a292-4bdf-b20c-a5c2a169ee84.jpg' },
-    prices: { eur: '38.82' },
+    prices: { eur: '38.10' },
     pagamos: 23
   },
   {
     name: 'Orcish Bowmasters',
     set_name: 'The Lord of the Rings: Tales of Middle-earth',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/7/c/7c024bae-5631-4e20-ac69-df392ac9e109.jpg' },
-    prices: { eur: '34.74' },
+    prices: { eur: '35.71' },
     pagamos: 21
   },
   {
     name: 'Rhystic Study',
     set_name: 'Prophecy',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/3/3/3394cefd-a3c6-4917-8f46-234e441ecfb6.jpg' },
-    prices: { eur: '34.37' },
+    prices: { eur: '35.31' },
     pagamos: 21
   },
   {
     name: 'Ragavan, Nimble Pilferer',
     set_name: 'Modern Horizons 2',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/a/9/a9738cda-adb1-47fb-9f4c-ecd930228c4d.jpg' },
-    prices: { eur: '33.76' },
+    prices: { eur: '32.19' },
     pagamos: 20
   },
   // Las tres de abajo salieron el 10/09/2026 en la revision de hotlists de fuera: Card
@@ -174,19 +174,19 @@ const CARTAS = [
     name: 'Ancient Tomb',
     set_name: 'Tempest',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/3/0/30e401e3-282b-4524-87e1-c6cd50cd6d00.jpg' },
-    prices: { eur: '109.55' }
+    prices: { eur: '111.86' }
   },
   {
     name: 'Wooded Foothills',
     set_name: 'Onslaught',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/c/d/cdad38f7-9dfa-4f1b-9fac-41ab2b253f53.jpg' },
-    prices: { eur: '87.13' }
+    prices: { eur: '90.04' }
   },
   {
     name: 'Food Chain',
     set_name: 'Mercadian Masques',
     image_uris: { normal: 'https://cards.scryfall.io/normal/front/1/8/18a1bb9e-006c-495e-8f99-d451183d2669.jpg' },
-    prices: { eur: '28.08' }
+    prices: { eur: '28.33' }
   }
 ];
 
